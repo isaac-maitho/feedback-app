d3d51bad3eb5c10c3b4231af79f529501150e8c6
wyclif-maitho <isaacmaitho14@gmail.com>
2023-03-15 20:13:44 +0300

    first commit for the app

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,15 @@
+import FeebackItem from "./FeebackItem"
+function FeedbackList({feedback}) {
+    if (!feedback || feedback.length ===0){
+        return <p>No Feedback yet</p>
+    }
+  return (
+    <div className="feedback-list">
+        {feedback.map((item) => (
+            <FeebackItem key ={item.id} item={item}/>
+        ))}
+    </div>
+  )
+}
+
+export default FeedbackList
---
+++ src/data/FeebackData.js
@@ -0,0 +1,18 @@
+const feedbackData = [
+    {
+        id:1,
+        rating: 8,
+        text:'The services were okay'
+    },
+    {
+        id:2,
+        rating: 5,
+        text: 'I had some complains with the reception'
+    },
+    {
+        id:3,
+        rating: 9,
+        text: 'The services were exceptional'
+    }
+]
+export default feedbackData;
