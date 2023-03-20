ee42631d219c5d8c5ca06f5d2646e946252e1e92
wyclif-maitho <isaacmaitho14@gmail.com>
2023-03-20 15:01:23 +0300

    more features added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import feedbackData from "./data/FeebackData";
 function App () { 
     const [feedback, setFeedback]= useState(feedbackData)
+    
+    const addFeedback =(newFeedback) =>{
+        newFeedback.id = uuidv4()
+        setFeedback ([newFeedback, ...feedback])
+    }
     
     const deleteFeedback =(id) =>{
         if(window.confirm('Are you sure you want to delete?')){
@@ -15,6 +22,7 @@ function App () {
         <>
         <Header />
         <div className="container">
+        <FeedbackForm handleAdd ={addFeedback}/>
         <FeedbackStats feedback ={feedback}/>
         <FeedbackList feedback ={feedback}
             handleDelete ={deleteFeedback}
