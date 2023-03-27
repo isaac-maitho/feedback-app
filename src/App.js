0afe7849292ca1d6a2d53b7d73eff9223591d17a
wyclif-maitho <isaacmaitho14@gmail.com>
2023-03-27 16:34:41 +0300

    added context API and useContext hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { v4 as uuidv4 } from "uuid";
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
-import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutIconLink from "./components/AboutIconLink";
-import feedbackData from "./data/FeebackData";
 import AboutPage from "./pages/AboutPage";
+import { FeebackProvider } from "./components/context/FeedbackContext";
 function App () { 
-    const [feedback, setFeedback]= useState(feedbackData)
-    
-    const addFeedback =(newFeedback) =>{
-        newFeedback.id = uuidv4()
-        setFeedback ([newFeedback, ...feedback])
-    }
-    
-    const deleteFeedback =(id) =>{
-        if(window.confirm('Are you sure you want to delete?')){
-            setFeedback(feedback.filter((item) =>item.id !== id))
-        }
-    }
     return (
+        <FeebackProvider>
         <Router>
           <Header />
           <div className="container">
           <Routes>
            <Route exact path="/" element = {
             <>
-             <FeedbackForm handleAdd ={addFeedback}/>
-             <FeedbackStats feedback ={feedback}/>
-             <FeedbackList feedback ={feedback}
-             handleDelete ={deleteFeedback}/>
+             <FeedbackForm/>
+             <FeedbackStats/>
+             <FeedbackList/>
             </>
            }>
            
@@ -41,6 +27,7 @@ function App () {
           <AboutIconLink/>
          </div>
         </Router>
+        </FeebackProvider>
     )
 }
 
